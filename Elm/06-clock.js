2bd04f133c8836db906c07a20ac6b6aaa00e528e
cronokirby <cronokirby@gmail.com>
2017-05-16 21:38:38 +0200

    adds the clock example

---
+++ Elm/06-clock.js
@@ -0,0 +1,34 @@
+new Vue({
+    el: "#app",
+    data: {
+      angle: 0
+    },
+    computed: {
+      handX: function () {
+        return 50 + 40 * Math.cos(this.angle)
+      },
+      handY: function () {
+        return 50 + 40 * Math.sin(this.angle)
+      }
+    },
+    methods: {
+      start: function () {
+        console.log("starting")
+        var vm = this
+        var tween = new TWEEN.Tween({x: 0})
+          .to({x: 60}, 60000) 
+          .onUpdate(function() {
+            // second => degree => radians
+            vm.angle = Math.floor(this.x) * Math.PI / 30 
+          })
+          .repeat(Infinity)
+          .start()
+
+        function animate(time) {
+          requestAnimationFrame(animate)
+          TWEEN.update(time)
+        }
+        requestAnimationFrame(animate)
+      }
+    }
+})
